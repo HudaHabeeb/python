ce2c69c923b40152162dabe9eb997bbc7078a336
HudaHabeeb <144018906+HudaHabeeb@users.noreply.github.com>
2025-01-01 16:01:26 +0530

    Add files via upload

---
+++ counta.py
@@ -0,0 +1,8 @@
+
+names=["Rohan","Sithara","Nayana","Kiara"]
+count=0
+for i in names:
+    for j in i:
+        if j=="a":
+            count=count++1
+            print("occurence of a list:",count)
---
+++ sort.py
@@ -0,0 +1,7 @@
+my_dict={'b': 2,'a': 1,'d': 4,'e': 3}
+asc_dict = dict(sorted(my_dict.items()))
+des_dict = dict(sorted(my_dict.items(),reverse=True))
+print("original Dictionary:", my_dict)
+print("Ascending order:", asc_dict)
+print("Descending order:", des_dict)
+
